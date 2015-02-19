544f929d23e3da161f6764fa38b261881f8d7741
Austin A Baty <abaty@hidsk0001.cmsaf.mit.edu>
2015-02-19 04:11:38 -0500

    added pp

--- residualCorrectionDerivationpp.C
+++ residualCorrectionDerivationpp.C
@@ -25,7 +25,7 @@
 #include "TCut.h"
 #include "TStyle.h"
 
-void residualCorrection()
+void residualCorrection(int cone = 3)
 {
   TH1::SetDefaultSumw2();
   TH2::SetDefaultSumw2();
@@ -36,7 +36,7 @@ void residualCorrection()
   const double ajBins[najBins+1] = {0,0.11,0.22,0.33,1};
   const double ptBins[nptBins+1] = {0.5,1,2,4,8,300};
 
-  TFile * f= new TFile("/export/d00/scratch/abaty/trackingEff/closure_ntuples/Correction_Vs3PF_ntuple_dijet_etaLT16_NoFFJEC.root","read");
+  TFile * f= new TFile(Form("/net/hidsk0001/d00/scratch/dgulhan/ntuples_res_trk_pp/track_ntuple_HiForest_Private_PYTHIA_localdb_ppJEC_pthat30_merged_forest_0_ak%dCalo.root",cone),"read");
   TTree * nt_track = (TTree*)f->Get("nt_track");
   TTree * nt_particle = (TTree*)f->Get("nt_particle");
   
@@ -46,15 +46,16 @@ void residualCorrection()
   TH2D * leadCorr_gen = new TH2D("leadCorr_gen",":A_{j}:p_{t}^{track}",najBins,ajBins,nptBins,ptBins);
   TH2D * subleadCorr_gen = new TH2D("subleadCorr_gen",":A_{j}:p_{t}^{track}",najBins,ajBins,nptBins,ptBins);
 
+  //removed "w_vz*weight" factor
   TCut cutReco_lead = "trackselect && pt>0.5 && r_lead<0.2";
   TCut cutReco_sublead = "trackselect && pt>0.5 && r_sublead<0.2";
-  nt_track->Draw("pt:asym>>leadCorr","((1-fake)/eff)*weight"*(cutReco_lead),"colz");
-  nt_track->Draw("pt:asym>>subleadCorr","((1-fake)/eff)*weight"*(cutReco_sublead),"colz");
+  nt_track->Draw("pt:-(asym)>>leadCorr","((1-secondary)*(1-fake)/((1+multrec)*eff))"*(cutReco_lead),"colz");
+  nt_track->Draw("pt:-(asym)>>subleadCorr","((1-secondary)*(1-fake)/((1+multrec)*eff))"*(cutReco_sublead),"colz");
 
   TCut cutGen_lead = "pt>0.5 && r_lead<0.2";
   TCut cutGen_sublead = "pt>0.5 && r_sublead<0.2";
-  nt_particle->Draw("pt:asym>>leadCorr_gen","weight"*(cutGen_lead),"colz");
-  nt_particle->Draw("pt:asym>>subleadCorr_gen","weight"*(cutGen_sublead),"colz");
+  nt_particle->Draw("pt:-(asym)>>leadCorr_gen",(cutGen_lead),"colz");
+  nt_particle->Draw("pt:-(asym)>>subleadCorr_gen",(cutGen_sublead),"colz");
 
   leadCorr->Divide(leadCorr_gen);
   subleadCorr->Divide(subleadCorr_gen);
@@ -74,8 +75,6 @@ void residualCorrection()
 
   TLegend * l3 = new TLegend(0.37,0.5,0.77,0.9);
   l3->AddEntry((TObject*)0,"Difference in Closures","");
-  l3->AddEntry((TObject*)0,"No Frag. JEC Applied","");
-  l3->AddEntry((TObject*)0,"akVs3PF jets","");
   l3->SetBorderSize(0);
   l3->SetTextSize(0.04);
 
@@ -86,8 +85,8 @@ void residualCorrection()
   leadCorr->GetXaxis()->SetTitle("A_{j}");
   leadCorr->Draw("colz");
   l1->Draw("same");
-  c2->SaveAs("plots/leading_correction_AkVs3PF_NoFFJEC.pdf");
-  c2->SaveAs("plots/leading_correction_AkVs3PF_NoFFJEC.png");
+  c2->SaveAs(Form("plots/leading_correction_AkVs%dCalo_pp.pdf",cone));
+  c2->SaveAs(Form("plots/leading_correction_AkVs%dCalo_pp.png",cone));
 
   TCanvas * c3 = new TCanvas("c3","c3",500,500);
   c3->SetRightMargin(0.15);
@@ -96,8 +95,8 @@ void residualCorrection()
   subleadCorr->GetXaxis()->SetTitle("A_{j}");
   subleadCorr->Draw("colz");
   l2->Draw("same");
-  c3->SaveAs("plots/subleading_correction_AkVs3PF_NoFFJEC.pdf");
-  c3->SaveAs("plots/subleading_correction_AkVs3PF_NoFFJEC.png");
+  c3->SaveAs(Form("plots/subleading_correction_AkVs%dCalo_pp.pdf",cone));
+  c3->SaveAs(Form("plots/subleading_correction_AkVs%dCalo_pp.png",cone));
 
   TCanvas * c4 = new TCanvas("c4","c4",500,500);
   c4->SetRightMargin(0.15);
@@ -106,10 +105,10 @@ void residualCorrection()
   diff->GetXaxis()->SetTitle("A_{j}");
   diff->Draw("colz");
   l3->Draw("same");
-  c4->SaveAs("plots/difference_AkVs3PF_NoFFJEC.pdf");
-  c4->SaveAs("plots/difference_AkVs3PF_NoFFJEC.png");
+  c4->SaveAs(Form("plots/difference_AkVs%dCalo_pp.pdf",cone));
+  c4->SaveAs(Form("plots/difference_AkVs%dCalo_pp.png",cone));
 
-  TFile * outf = new TFile("rootFiles/ResidualTrkCorr_AkVs3PF_NoFFJEC.root","recreate");
+  TFile * outf = new TFile(Form("rootFiles/ResidualTrkCorr_AkVs%dCalo_pp.root",cone),"recreate");
   leadCorr->SetDirectory(0);
   subleadCorr->SetDirectory(0);
   leadCorr->Write();
